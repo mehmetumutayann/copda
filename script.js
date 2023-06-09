dd7f47869ae3b425e80b1c2cbefe88592a976b2f
Furkan <furkanfc72008@gmail.com>
2023-06-09 10:25:01 +0300

    Local Storage Kaydedilme Kodu

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+// Kişinin yazdığı mesajı infocopda@gmail.com adresine mail atmasını sağlar
 document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault(); 
+    event.preventDefault(); // Sayfanın yeniden yüklenmesini engeller
   
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
@@ -7,10 +8,31 @@ document.getElementById("contact-form").addEventListener("submit", function(even
   
     var body = "Ad: " + name + "\nE-posta: " + email + "\n\n" + message;
   
+    // E-posta adresini belirler
     var recipient = "infocopda@gmail.com";
     var subject = "Yeni İletişim Mesajı";
   
     var mailtoLink = "mailto:" + recipient + "?subject=" + encodeURIComponent(subject) + "&body=" + encodeURIComponent(body);
   
     window.location.href = mailtoLink;
-});
+  });
+
+
+
+  // Kişinin yazdığı bilgileri local storage e kaydeder
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Sayfanın yeniden yüklenmesini engelle
+  
+    // Formdaki girdileri al
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+  
+    // Girdileri yerel depolamaya kaydet
+    localStorage.setItem("name", name);
+    localStorage.setItem("email", email);
+    localStorage.setItem("message", message);
+  
+    this.reset();
+  });
+  
